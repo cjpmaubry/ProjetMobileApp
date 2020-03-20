64c13e11d64d1ad04d0e4dbe05d2242e10759691
cjpmaubry <cjpm.aubry@gmail.com>
2020-03-20 17:33:07 +0100

    Added layout of nodepad

--- app/src/main/java/com/modildev/mytestapplication1/NotepadActivity.java
+++ app/src/main/java/com/modildev/mytestapplication1/NotepadActivity.java
@@ -1,30 +1,67 @@
 package com.modildev.mytestapplication1;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-public class NotepadActivity extends AppCompatActivity {
+public class NotepadActivity extends Activity {
+
+    Button newButton,saveButton,openButton;
+    EditText text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-        BottomNavigationView navView = findViewById(R.id.nav_view2);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home2, R.id.navigation_notepad, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment2);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        setContentView(R.layout.activity_notepad);
+
+        newButton=(Button)findViewById(R.id.newButton);
+        saveButton=(Button)findViewById(R.id.saveButton);
+        text=(EditText)findViewById(R.id.text);
     }
 
+    public void buttonAction(View v) {
+        final EditText fileName=new EditText(this);
+        AlertDialog.Builder ad=new AlertDialog.Builder(this);
+        ad.setView(fileName);
+
+        if (v.getId() == R.id.saveButton) {
+            ad.setMessage("Save File");
+
+            ad.setPositiveButton("Save",new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    try {
+                        FileOutputStream fout=openFileOutput(fileName.getText().toString()+".txt",MODE_WORLD_READABLE);
+                        fout.write(text.getText().toString().getBytes());
+                    } catch (Exception e) {
+                        Toast.makeText(getApplicationContext(), "Error Occured: "+e,Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+
+            ad.setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+
+            ad.show();
+
+        }
+
+
+        if(v.getId()==R.id.newButton) {
+            text.setText("");
+        }
+    }
 }
