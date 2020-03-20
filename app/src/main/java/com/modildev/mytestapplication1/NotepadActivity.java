4d0b6bb580c10fb28feb044e21a6b0fd7657aead
cjpmaubry <cjpm.aubry@gmail.com>
2020-03-20 16:07:07 +0100

    Added Notepad Activity

---
+++ app/src/main/java/com/modildev/mytestapplication1/NotepadActivity.java
@@ -0,0 +1,30 @@
+package com.modildev.mytestapplication1;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class NotepadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        BottomNavigationView navView = findViewById(R.id.nav_view2);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home2, R.id.navigation_notepad, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment2);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+
+}
--- app/src/main/java/com/modildev/mytestapplication1/ui/notepad/NotepadFragment.java
+++ app/src/main/java/com/modildev/mytestapplication1/ui/notepad/NotepadFragment.java
@@ -1,9 +1,12 @@
 package com.modildev.mytestapplication1.ui.notepad;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +15,9 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.modildev.mytestapplication1.MainActivity;
+import com.modildev.mytestapplication1.MainActivity2;
+import com.modildev.mytestapplication1.NotepadActivity;
 import com.modildev.mytestapplication1.R;
 
 public class NotepadFragment extends Fragment {
@@ -23,13 +29,24 @@ public class NotepadFragment extends Fragment {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(NotepadViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notepad, container, false);
-        final TextView textView = root.findViewById(R.id.text_notepad);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
+
+        initUI(root);
+        return(root);
+    }
+
+    private void initUI(View v) {
+        Button button = (Button) v.findViewById(R.id.button_notepad);
+        button.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                // Change the Activity
+                Intent intent = new Intent(getActivity(), NotepadActivity.class);
+                startActivity(intent);
+
             }
         });
-        return root;
+
+
     }
 }
